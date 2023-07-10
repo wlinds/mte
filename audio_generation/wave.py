25dbc7f66e0814fcccb8ac52997da9b95337f803
William L <41084946+helveticae@users.noreply.github.com>
2023-07-10 23:33:51 +0200

    add support for sine and square waves

--- audio_generation/wave.py
+++ audio_generation/wave.py
@@ -1,21 +1,38 @@
 import numpy as np
 import sounddevice as sd
-import matplotlib.pyplot as plt
 
-# sampling rate and the duratio
-fs = 44100 # Hz
-duration = 5 # seconds
+def generate_waveform(frequency, waveform_type, duration=1, sampling_rate=44100) -> np.ndarray:
+    """
+    Generates a waveform of a specified frequency and type. Supports sine and square.
+    #TODO: Add sawtooth?
+    """
 
-# time array from 0 to duration
-t = np.linspace(0, duration, int(duration * fs), endpoint=False)
+    # time array from 0 to duration
+    t = np.linspace(0, duration, int(duration * sampling_rate), endpoint=False)
 
-# Sawtooth wave @ 440 Hz
-f = 440
-x = np.mod(f * t, 1) - 0.5
+    if waveform_type == 'sine':
+        x = np.sin(2 * np.pi * frequency * t)
+    elif waveform_type == 'square':
+        x = np.sign(np.sin(2 * np.pi * frequency * t))
+    else:
+        raise ValueError('Choose either "sine" or "square".')
 
-# Normalize
-x = x / np.max(np.abs(x))
+    # Normalize
+    x = x / np.max(np.abs(x))
+
+    return x
 
 if __name__ == "__main__":
-    sd.play(x, fs)
+    # Sine wave @ 440 Hz
+    f = 440
+    x = generate_waveform(f, 'sine')
+    sd.play(x)
+    
+    sd.wait()
+
+    # Square wave @ 880 Hz
+    f = 880
+    x = generate_waveform(f, 'square')
+    sd.play(x)
+
     sd.wait()
