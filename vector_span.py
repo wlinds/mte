5565417c5fdfd2a453f89ee4490c0e66010fe187
William L <41084946+helveticae@users.noreply.github.com>
2023-08-07 13:48:16 +0200

    add list of drawn vectors, add vector name generation

--- vector_span.py
+++ vector_span.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame.font
+import random
 
 pygame.init()
 screen_x, screen_y = 640, 640
@@ -12,10 +13,18 @@ pygame.draw.line(screen, (140, 140, 140), (center_x, 0), (center_x, screen_y), 1
 pygame.draw.line(screen, (140, 140, 140), (0, center_y), (screen_x, center_y), 1)
 
 vectors = []
+vector_name= []
 
 def add_vector(pos1, pos2):
     vector = [pos2[0] - pos1[0], pos2[1] - pos1[1]]
+
+    var_name = chr(random.randint(65, 90)).lower()
+    while var_name in vector_name:
+        var_name = chr(random.randint(65, 90)).lower() # reroll if exist
+
+    vector_name.append(var_name)
     vectors.append(vector)
+    print(var_name, vector)
 
 def draw_vectors():
     for vector in vectors:
@@ -25,6 +34,17 @@ def draw_vectors():
         text_surface = font.render(text, True, (255, 255, 255))
         screen.blit(text_surface, (x, y))
         pygame.draw.line(screen, (255, 255, 255), (center_x, center_y), (x, y), 1)
+
+def draw_v_list():
+    x, y = 10, 10
+    width, height = 100, 25
+    y_offset = 0
+    for index, vector in enumerate(vectors):
+        text = f"({vector_name[index]}: {vector[0]}, {-vector[1]})"
+        font = pygame.font.SysFont("Courier New", 12)
+        text_surface = font.render(text, True, (80, 80, 240))
+        screen.blit(text_surface, (x + 10, y + 10 + y_offset))
+        y_offset += 15
 
 while True:
     for event in pygame.event.get():
@@ -39,5 +59,6 @@ while True:
             pos2 = pygame.mouse.get_pos()
             add_vector(pos1, pos2)
 
+    draw_v_list()
     draw_vectors()
     pygame.display.update()
