b71999080ec9d2bfc8a4dcf322e7802b9eee485e
William L <41084946+helveticae@users.noreply.github.com>
2023-08-25 19:46:59 +0200

    render additional rotation info, improved comments

--- openGL_grid.py
+++ openGL_grid.py
@@ -4,24 +4,19 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
-def draw_text(position, text):
-    glPushMatrix()
-    glTranslatef(*position)
-    glRasterPos3f(0, 0, 0)
-    for char in text:
-        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ord(char))
-    glPopMatrix()
-
 pygame.init()
 display = (800, 600)
 screen = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
-
+# !! FOV also affects text position on screen.
 gluPerspective(50, (display[0] / display[1]), 0.1, 50.0) # Field of View, Aspect Ratio, zNear & zFar (both clipping distace)
 glTranslatef(0.0, 0.0, -5) # Move grid on negative z axis
 
 grid_position_z = 0.0
-grid_speed = 0.000
+grid_speed = 0.0
+
+base_rotation_speed = 1.0
+slow_rotation_factor = 0.5 # Currently not implemented, but could be used to lower rotation speed
 
 rotation_angle_x = 0.0
 rotation_speed = 1.0
@@ -32,8 +27,48 @@ rotation_speed_z = 1.0
 rotation_angle_y = 0.0
 rotation_speed_y = 1.0
 
-dragging = False # Used for detecting mouse click (click and hold to change grid_speed variable)
+dragging = False # Used for detecting mouse click (click and hold to change grid_speed variable) #TODO: Change keybind?
 drag_start_y = 0
+
+def draw_text(position, text):
+    glPushMatrix()
+    glTranslatef(*position)
+    glRasterPos3f(0, 0, 0)
+    for char in text:
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ord(char))
+    glPopMatrix()
+
+def display_rotation_combination():
+    #TODO: Simplified as rotation matrix, maybe?
+    coefficients = {
+        "x": rotation_angle_x,
+        "y": rotation_angle_y,
+        "z": rotation_angle_z,
+    }
+
+    rotations = {symbol: angle % 360 for symbol, angle in coefficients.items()}    
+    non_zero = {symbol: angle for symbol, angle in rotations.items() if angle != 0}
+    
+    if non_zero:
+        rotation_combination = " + ".join([f"({symbol}: {angle:.2f}°)" for symbol, angle in non_zero.items()])
+    else:
+        rotation_combination = "No Rotation"
+    
+    return rotation_combination
+
+# Function to map key_names to pressed key (only used for UI)
+def get_pressed_keys():
+    pressed_keys = [key_names[key] for key in key_names if keys[key]]
+    return "+".join(pressed_keys)
+
+key_names = {
+    pygame.K_q: "Q",
+    pygame.K_e: "E",
+    pygame.K_d: "D",
+    pygame.K_a: "A",
+    pygame.K_s: "S",
+    pygame.K_w: "W",
+}
 
 while True:
     for event in pygame.event.get():
@@ -47,6 +82,10 @@ while True:
             dragging = False
         
     keys = pygame.key.get_pressed()
+
+    # Only used for displaying a combinations of keys currently pressed
+    pressed_keys_text = get_pressed_keys()
+
     if keys[pygame.K_q]:
         rotation_angle_x += rotation_speed  # Counter-clockwise rotation
     if keys[pygame.K_e]:
@@ -66,6 +105,7 @@ while True:
         mouse_y = pygame.mouse.get_pos()[1]
         scroll = drag_start_y - mouse_y
         grid_speed += scroll * 0.00001  # Mouse movement sensitivity setting
+
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Clear buffers at beginning of each frame
     glPushMatrix()
@@ -92,13 +132,17 @@ while True:
     glEnd()
 
     glPopMatrix() # Pop (glPushMatrix) before rotating (otherwise crashes with error 1283, b'stack overflow' (?))
-    
 
     # Render text (zoom level and rotation)
-    text_position = (-4.5, -4.5, -5)
-    zoom_text = f"(Mouse) Zoom Level: {grid_position_z:.2f} | (Q/R) Rotation Level: {rotation_angle_x:.2f}"
+    # TODO: Text position should not be hardcoded like this to account for different screen size preferences,
+    # rn it's hardcoded to a classic VGA 800x600 window. This is an UI issue and will not be prioritized.
+
+    rotation_text = display_rotation_combination()
+    text_position = (-6, -4.5, -5)
+    bottom_text = f"Zoom Level (Mouse): {grid_position_z:.2f} | Rotation (WASD): {rotation_text}"
     glColor3f(1.0, 1.0, 1.0)
-    draw_text(text_position, zoom_text)
+    draw_text(text_position, bottom_text)
+    draw_text((-2,1.5,1.5), f"{pressed_keys_text}")
 
     pygame.display.flip()
     pygame.time.wait(10)
