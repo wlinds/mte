0cd8814a7ae85eaef2c93f03be54f64a7eb494da
William L <41084946+helveticae@users.noreply.github.com>
2023-08-26 11:12:00 +0200

    Total refactor (class implementation)

--- openGL_grid.py
+++ openGL_grid.py
@@ -4,222 +4,197 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
-pygame.init()
-display = (800, 600)
-screen = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+class Main:
+    def __init__(self):
+        pygame.init()
+        self.display = (800, 600) # TODO: adjust text render position for different display sizes
+        self.screen = pygame.display.set_mode(self.display, DOUBLEBUF | OPENGL)
 
-# !! FOV also affects text position on screen.
-gluPerspective(50, (display[0] / display[1]), 0.1, 50.0) # Field of View, Aspect Ratio, zNear & zFar (both clipping distace)
-glTranslatef(0.0, 0.0, -5) # Move grid on negative z axis
+        # !! FOV also affects text position on screen.
+        gluPerspective(50, (self.display[0] / self.display[1]), 0.1, 50.0) # FOV, Aspect Ratio, zNear & zFar (both clipping distace)
+        glTranslatef(0.0, 0.0, -5)
 
-grid_size = 16 # determines plane size along x and y axis (size 4 will create an 8x8 grid)
-grid_spacing = 0.5
-grid_position_z = 0.0
-grid_speed = 0.0 # This isn't really grid speed anymore, it's z level of the cube
+        self.grid_size = 16 # Set plane size (extrudes along x and y axis) (size 16 will create a 32x32 grid)
+        self.grid_spacing = 0.5
+        self.grid_position_z = 0.0
+        self.camera_speed_z = 0.0 # Camera z level movement speed
 
-rotation_speed = 1.0
-rotation_angle_x = 0.0
-rotation_angle_z = 0.0
-rotation_angle_y = 0.0
+        self.rotation_speed = 1.0
+        self.rotation_angle_x = 0.0
+        self.rotation_angle_z = 0.0
+        self.rotation_angle_y = 0.0
 
-dragging = False # Used for detecting mouse click (click and hold to change grid_speed variable) #TODO: Change keybind?
-drag_start_y = 0
+        self.dragging = False # Detecting mouse click (click and hold to change camera_speed_z) #TODO: Change keybind?
+        self.drag_start_y = 0
 
-# Draw semi-static text
-def draw_text(position, text):
-    glPushMatrix()
-    glTranslatef(*position)
-    glRasterPos3f(0, 0, 0)
-    for char in text:
-        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ord(char))
-    glPopMatrix()
+        self.cube_loc = (0.25, 0.25, 0.25)
 
-# Follow rotation (for cube and other objects)
-def follow_text(position, text, color=(1.0, 1.0, 1.0)):
-    glPushMatrix()
-    glTranslatef(*position)
-    glRotatef(rotation_angle_x, 1, 0, 0)
-    glRotatef(rotation_angle_y, 0, 1, 0)
-    glRotatef(rotation_angle_z, 0, 0, 1)
-    glColor3f(*color)  # Set text color
-    glRasterPos3f(0, 0, 0)
-    for char in text:
-        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ord(char))
-    glPopMatrix()
+        # Dict mapping used only for displaying player input
+        self.key_names = {
+            pygame.K_q: "Q",
+            pygame.K_e: "E",
+            pygame.K_d: "D",
+            pygame.K_a: "A",
+            pygame.K_s: "S",
+            pygame.K_w: "W",
+            pygame.K_LEFT: "left",
+            pygame.K_RIGHT: "right",
+            pygame.K_UP: "up",
+            pygame.K_DOWN: "down",
+        }
 
+    # Semi-static text (GUI)
+    def draw_text(self, position, text):
+        glPushMatrix()
+        glTranslatef(*position)
+        glRasterPos3f(0, 0, 0)
+        for char in text:
+            glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ord(char))
+        glPopMatrix()
 
-def draw_stupid_cube(position, size):
-    # Stupid cube vertices
-    u = size * 0.5
-    vertices = [
-        [ u,  u,  u],
-        [-u,  u,  u],
-        [-u, -u,  u],
-        [ u, -u,  u],
-        [ u,  u, -u],
-        [-u,  u, -u],
-        [-u, -u, -u],
-        [ u, -u, -u]
-    ]
-    
-    # Stupid cube corner connections
-    faces = [
-        [0, 1, 2, 3],
-        [3, 2, 6, 7],
-        [7, 6, 5, 4],
-        [4, 5, 1, 0],
-        [0, 3, 7, 4],
-        [1, 5, 6, 2]
-    ]
+    # Follow rotation and position (for cube and other objects, GUI)
+    def follow_text(self, position, text, color=(1.0, 1.0, 1.0)):
+        glPushMatrix()
+        glTranslatef(*position)
+        glRotatef(self.rotation_angle_x, 1, 0, 0)
+        glRotatef(self.rotation_angle_y, 0, 1, 0)
+        glRotatef(self.rotation_angle_z, 0, 0, 1)
+        glColor3f(*color)
+        glRasterPos3f(0, 0, 0)
+        for char in text:
+            glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ord(char))
+        glPopMatrix()
 
-    glBegin(GL_QUADS)
-    for stupid_face in faces:
-        for vertex_index in stupid_face:
-            vertex = vertices[vertex_index]
-            glVertex3fv([vertex[0] + position[0], vertex[1] + position[1], vertex[2] + position[2]])
-    glEnd()
+    def draw_stupid_cube(self, position, size):
+        # Stupid cube vertices
+        u = size * 0.5
+        vertices = [
+            [u, u, u], [-u, u, u], [-u, -u, u], [u, -u, u],
+            [u, u, -u], [-u, u, -u], [-u, -u, -u], [u, -u, -u]
+        ]
+        # Stupid cube corner connections
+        faces = [
+            [0, 1, 2, 3], [3, 2, 6, 7], [7, 6, 5, 4],
+            [4, 5, 1, 0], [0, 3, 7, 4], [1, 5, 6, 2]
+        ]
+        glBegin(GL_QUADS)
+        for stupid_face in faces:
+            for vertex_index in stupid_face:
+                vertex = vertices[vertex_index]
+                glVertex3fv([vertex[0] + position[0], vertex[1] + position[1], vertex[2] + position[2]])
+        glEnd()
 
-global cube_loc # testing with global for now, replace with class later
-cube_loc = (0.25, 0.25, 0.25) # defalut 
+    def move_on_grid(self, direction):
+        step = self.grid_spacing
+        if direction == "left":
+            self.cube_loc = (self.cube_loc[0] - step, self.cube_loc[1], self.cube_loc[2])
+        elif direction == "right":
+            self.cube_loc = (self.cube_loc[0] + step, self.cube_loc[1], self.cube_loc[2])
+        elif direction == "up":
+            self.cube_loc = (self.cube_loc[0], self.cube_loc[1] + step, self.cube_loc[2])
+        elif direction == "down":
+            self.cube_loc = (self.cube_loc[0], self.cube_loc[1] - step, self.cube_loc[2])
 
-def move_cube_on_grid(direction):
-    global cube_loc
-    step = grid_spacing
-    if direction == "left":
-        cube_loc = (cube_loc[0] - step, cube_loc[1], cube_loc[2])
-    elif direction == "right":
-        cube_loc = (cube_loc[0] + step, cube_loc[1], cube_loc[2])
-    elif direction == "up":
-        cube_loc = (cube_loc[0], cube_loc[1] + step, cube_loc[2])
-    elif direction == "down":
-        cube_loc = (cube_loc[0], cube_loc[1] - step, cube_loc[2])
+    def display_rotation_combination(self):
+        #TODO: Simplified as rotation matrix, maybe?
+        coefficients = {
+            "x": self.rotation_angle_x,
+            "y": self.rotation_angle_y,
+            "z": self.rotation_angle_z,
+        }
 
-def display_rotation_combination():
-    #TODO: Simplified as rotation matrix, maybe?
-    coefficients = {
-        "x": rotation_angle_x,
-        "y": rotation_angle_y,
-        "z": rotation_angle_z,
-    }
+        rotations = {symbol: angle % 360 for symbol, angle in coefficients.items()}
+        non_zero = {symbol: angle for symbol, angle in rotations.items() if angle != 0}
 
-    rotations = {symbol: angle % 360 for symbol, angle in coefficients.items()}    
-    non_zero = {symbol: angle for symbol, angle in rotations.items() if angle != 0}
-    
-    if non_zero:
-        rotation_combination = " + ".join([f"({symbol}: {angle:.2f}°)" for symbol, angle in non_zero.items()])
-    else:
-        rotation_combination = "No Rotation"
-    
-    return rotation_combination
+        if non_zero:
+            rotation_combination = " + ".join([f"({symbol}: {angle:.2f}°)" for symbol, angle in non_zero.items()])
+        else:
+            rotation_combination = "No Rotation"
 
-# Function to map key_names to pressed key (only used for UI)
-def get_pressed_keys():
-    pressed_keys = [key_names[key] for key in key_names if keys[key]]
-    return "+".join(pressed_keys)
+        return rotation_combination
 
-key_names = {
-    pygame.K_q: "Q",
-    pygame.K_e: "E",
-    pygame.K_d: "D",
-    pygame.K_a: "A",
-    pygame.K_s: "S",
-    pygame.K_w: "W",
-    pygame.K_LEFT: "left",
-    pygame.K_RIGHT: "right",
-    pygame.K_UP: "up",
-    pygame.K_DOWN: "down",
-}
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.dragging = True
+                    self.drag_start_y = event.pos[1]
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    self.dragging = False
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            dragging = True
-            drag_start_y = event.pos[1]
-        elif event.type == pygame.MOUSEBUTTONUP:
-            dragging = False
-        
-    keys = pygame.key.get_pressed()
+            # Display all keys currently pressed
+            keys = pygame.key.get_pressed()
+            pressed_keys_text = "+".join([self.key_names[key] for key in self.key_names if keys[key]])
 
-    # Only used for displaying a combinations of keys currently pressed
-    pressed_keys_text = get_pressed_keys()
+            self.rotation_speed = 1.0
+            self.rotation_angle_x += self.rotation_speed * (keys[pygame.K_q] - keys[pygame.K_e])
+            self.rotation_angle_z += self.rotation_speed * (keys[pygame.K_d] - keys[pygame.K_a])
+            self.rotation_angle_y += self.rotation_speed * (keys[pygame.K_s] - keys[pygame.K_w])
 
-    if keys[pygame.K_q]:
-        rotation_angle_x += rotation_speed  # Counter-clockwise rotation
-    if keys[pygame.K_e]:
-        rotation_angle_x -= rotation_speed  # Clockwise rotation
+            if keys[pygame.K_LEFT]:
+                self.move_on_grid("left")
+            if keys[pygame.K_RIGHT]:
+                self.move_on_grid("right")
+            if keys[pygame.K_UP]:
+                self.move_on_grid("up")
+            if keys[pygame.K_DOWN]:
+                self.move_on_grid("down")
 
-    if keys[pygame.K_d]:
-        rotation_angle_z += rotation_speed  # Counter-clockwise rotation
-    if keys[pygame.K_a]:
-        rotation_angle_z -= rotation_speed  # Clockwise rotation
+            if self.dragging:
+                mouse_y = pygame.mouse.get_pos()[1]
+                scroll = self.drag_start_y - mouse_y
+                self.camera_speed_z += scroll * 0.00001
 
-    if keys[pygame.K_s]:
-        rotation_angle_y += rotation_speed  # Counter-clockwise rotation
-    if keys[pygame.K_w]:
-        rotation_angle_y -= rotation_speed  # Clockwise rotation
-    
-    # Move cube along grid
-    if keys[pygame.K_LEFT]:
-        move_cube_on_grid("left")
-    if keys[pygame.K_RIGHT]:
-        move_cube_on_grid("right")
-    if keys[pygame.K_UP]:
-        move_cube_on_grid("up")
-    if keys[pygame.K_DOWN]:
-        move_cube_on_grid("down")
-    
-    if dragging:
-        mouse_y = pygame.mouse.get_pos()[1]
-        scroll = drag_start_y - mouse_y
-        grid_speed += scroll * 0.00001  # Mouse movement sensitivity setting
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+            glPushMatrix()
+            glRotatef(self.rotation_angle_x, 0.0, 0.0, 1.0)
+            glRotatef(self.rotation_angle_z, 0.0, 1.0, 0.0)
+            glRotatef(self.rotation_angle_y, 1.0, 0.0, 0.0)
+            glTranslatef(0, 0, self.camera_speed_z)
+            self.grid_position_z += self.camera_speed_z
 
+            # Define wireframe grid
+            glBegin(GL_LINES)
+            glColor3f(0.0, 0.5, 0.5)
+            spacing = self.grid_spacing
+            size = self.grid_size
+            for i in range(-size, size + 1):
+                glVertex3f(i * spacing, size * spacing, self.grid_position_z)
+                glVertex3f(i * spacing, -size * spacing, self.grid_position_z)
+                glVertex3f(size * spacing, i * spacing, self.grid_position_z)
+                glVertex3f(-size * spacing, i * spacing, self.grid_position_z)
+            glEnd()
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Clear buffers at beginning of each frame
-    glPushMatrix()
-    glRotatef(rotation_angle_x, 0.0, 0.0, 1.0)
-    glRotatef(rotation_angle_z, 0.0, 1.0, 0.0)
-    glRotatef(rotation_angle_y, 1.0, 0.0, 0.0)
-    glTranslatef(0, 0, grid_speed)
-    grid_position_z += grid_speed
+            # Draw the line from origin to cube
+            distance_to_cube = ((self.cube_loc[0] ** 2) + (self.cube_loc[1] ** 2) + (self.cube_loc[2] ** 2)) ** 0.5
+            distance_text = f"Distance: {distance_to_cube:.2f}"
+            self.draw_text((self.cube_loc[0] * 0.5, self.cube_loc[1] * 0.5, self.cube_loc[2] * 0.5), distance_text)
 
-    # Define wireframe grid
-    glBegin(GL_LINES)
-    glColor3f(0.0, 0.5, 0.5)
+            cube_size = self.grid_spacing
+            self.draw_stupid_cube(self.cube_loc, cube_size)
 
-    spacing = grid_spacing
-    size = grid_size
-    
-    for i in range(-size, size + 1):
-        glVertex3f(i * spacing, size * spacing, grid_position_z)
-        glVertex3f(i * spacing, -size * spacing, grid_position_z)
-        glVertex3f(size * spacing, i * spacing, grid_position_z)
-        glVertex3f(-size * spacing, i * spacing, grid_position_z)
+            self.follow_text((self.cube_loc[0] - 0.3, self.cube_loc[1], self.cube_loc[2]), "Stupid cube #1")
 
-    glEnd()
+            glPopMatrix() # Pop (glPushMatrix) before rotating (otherwise crashes with error 1283, b'stack overflow' (?))
 
 
-    cube_size = grid_spacing
-    draw_stupid_cube(cube_loc, cube_size)
+            # Render text (zoom level and rotation)
+            # TODO: Text position should not be hardcoded like this to account for different screen size preferences,
+            # rn it's hardcoded to a classic VGA 800x600 window. This is an UI issue and will not be prioritized.
+            rotation_text = self.display_rotation_combination()
+            text_position = (-6, -4.5, -5)
+            bottom_text = f"Zoom Level (Mouse): {self.grid_position_z:.2f} | Rotation (WASD+QE): {rotation_text}"
+            glColor3f(1.0, 1.0, 1.0)
+            self.draw_text(text_position, bottom_text)
 
-    follow_text((cube_loc[0] - 0.3, cube_loc[1], cube_loc[2]), "Stupid cube #1")
+            self.draw_text((-2, 1.5, 1.5), pressed_keys_text)
 
-    glPopMatrix() # Pop (glPushMatrix) before rotating (otherwise crashes with error 1283, b'stack overflow' (?))
+            pygame.display.flip()
+            pygame.time.wait(10)
 
-    # Render text (zoom level and rotation)
-    # TODO: Text position should not be hardcoded like this to account for different screen size preferences,
-    # rn it's hardcoded to a classic VGA 800x600 window. This is an UI issue and will not be prioritized.
-
-    rotation_text = display_rotation_combination()
-    text_position = (-6, -4.5, -5)
-    bottom_text = f"Zoom Level (Mouse): {grid_position_z:.2f} | Rotation (WASD+QE): {rotation_text}"
-    glColor3f(1.0, 1.0, 1.0)
-    draw_text(text_position, bottom_text)
-
-    # Key currently pressed
-    draw_text((-2,1.5,1.5), f"{pressed_keys_text}")
-
-    pygame.display.flip()
-    pygame.time.wait(10)
+if __name__ == "__main__":
+    app = Main()
+    app.run()
